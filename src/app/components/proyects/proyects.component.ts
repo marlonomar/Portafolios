cf05d393fef02d5a48ceb7f54eb9121dd9023bec
marlon <marlon.omar.g.garcia@gmail.com>
2020-05-23 23:58:21 -0300

    ajuste mobile

--- src/app/components/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects.component.ts
@@ -31,7 +31,6 @@ export class ProyectsComponent implements OnInit {
     this.proyects = [
       new Proyect ('Pokemon Battle',route+'pokemon.PNG','https://website-pokebattle.herokuapp.com/','simples'),
       new Proyect ('Cafeteria Guerrero',route+'cafeteria.PNG','https://marlonomar.github.io/cafeteria/','simples'),
-      new Proyect ('Cafeteria Guerrero',route+'cafeteria.PNG','https://marlonomar.github.io/cafeteria/','simples'),
       new Proyect ('Formulario Venezuela',route+'formulario.PNG','https://marlonomar.github.io/formulario-ve/','simples'),
       new Proyect ('Lista de Usuarios',route+'lista-usuarios.PNG','https://marlonomar.github.io/lista-de-usuarios/','simples')
     ];
